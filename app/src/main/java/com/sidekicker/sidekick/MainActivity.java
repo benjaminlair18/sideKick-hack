fe353f18ab0207b3551369d903af18d82f25a869
benjaminlair18 <benr_18@hotmail.com>
2016-04-16 12:15:47 +0800

    add button

--- app/src/main/java/com/sidekicker/sidekick/MainActivity.java
+++ app/src/main/java/com/sidekicker/sidekick/MainActivity.java
@@ -1,14 +1,45 @@
 package com.sidekicker.sidekick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.tutorial).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TutorialActivity.class));
+            }
+        });
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        findViewById(R.id.map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+
     }
+
+
+
 }
