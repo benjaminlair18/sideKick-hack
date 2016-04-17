ccff2876b39f85f7f162d2ee230f68af9c6a15cc
benjaminlair18 <benr_18@hotmail.com>
2016-04-17 10:05:33 +0800

    add string

--- app/src/main/java/com/sidekicker/sidekick/MainMapsActivity.java
+++ app/src/main/java/com/sidekicker/sidekick/MainMapsActivity.java
@@ -10,6 +10,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -47,6 +50,29 @@ public class MainMapsActivity
 		return false;
 	}
 
+	public void on99(View view)
+	{
+
+		RadioGroup rg1 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg1);
+		RadioGroup rg2 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg2);
+		RadioGroup rg3 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg3);
+		RadioGroup rg4 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg4);
+		RadioGroup rg5 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg5);
+		RadioGroup rg6 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg6);
+		RadioGroup rg7 = (RadioGroup) UserInputActivity.inputV.findViewById(R.id.rg7);
+
+		String radiovalue1 = ((RadioButton)UserInputActivity.inputV.findViewById(rg1.getCheckedRadioButtonId())).getText().toString();
+		String radiovalue2 = ((RadioButton)UserInputActivity.inputV.findViewById(rg2.getCheckedRadioButtonId())).getText().toString();
+		String radiovalue3 = ((RadioButton)UserInputActivity.inputV.findViewById(rg3.getCheckedRadioButtonId())).getText().toString();
+		String radiovalue4 = ((RadioButton)UserInputActivity.inputV.findViewById(rg4.getCheckedRadioButtonId())).getText().toString();
+		String radiovalue5 = ((RadioButton)UserInputActivity.inputV.findViewById(rg5.getCheckedRadioButtonId())).getText().toString();
+		String radiovalue6 = ((RadioButton)UserInputActivity.inputV.findViewById(rg6.getCheckedRadioButtonId())).getText().toString();
+		String radiovalue7 = ((RadioButton)UserInputActivity.inputV.findViewById(rg7.getCheckedRadioButtonId())).getText().toString();
+
+
+
+
+	}
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
 	 * sections of the app.
--- app/src/main/java/com/sidekicker/sidekick/UserInputActivity.java
+++ app/src/main/java/com/sidekicker/sidekick/UserInputActivity.java
@@ -1,11 +1,14 @@
 package com.sidekicker.sidekick;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
@@ -19,14 +22,17 @@ public class UserInputActivity extends Fragment
 
         static public StreetViewPanoramaView mStreetViewPanoramaView;
         static public StreetViewPanorama mPanorama;
+        static  public  View inputV;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View v = inflater.inflate(R.layout.activity_user_input, null);
+        inputV = inflater.inflate(R.layout.activity_user_input, null);
 
         Marker info = MainMapsActivity.inputMarker;
 
-        viewtext = (TextView)v.findViewById(R.id.textid);
+        viewtext = (TextView)inputV.findViewById(R.id.textid);
+
+
 
         if(info == null)
         {
@@ -37,7 +43,7 @@ public class UserInputActivity extends Fragment
 
         }
 
-                mStreetViewPanoramaView = (StreetViewPanoramaView) v.findViewById(R.id.steet_view_panorama);
+                mStreetViewPanoramaView = (StreetViewPanoramaView) inputV.findViewById(R.id.steet_view_panorama);
                 mStreetViewPanoramaView.onCreate(savedInstanceState);
                 mStreetViewPanoramaView.getStreetViewPanoramaAsync(new OnStreetViewPanoramaReadyCallback() {
                         @Override
@@ -49,7 +55,16 @@ public class UserInputActivity extends Fragment
 
                             });
 
-        return v;
+        return inputV;
+
+    }
+
+
+    public void onresult(View view)
+    {
+
+
+
 
     }
 }
